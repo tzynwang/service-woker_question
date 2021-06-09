7f8e324c5a2a43fbbbc59b675603cbb760d3067d
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-06-09 09:35:54 +0800

    test: log fetch(event.request.url)

--- sw.js
+++ sw.js
@@ -25,10 +25,12 @@ self.addEventListener('fetch', event => {
     }
 
     if (event.request.url.includes('randomuser.me/api/portraits')) {
-      const fetchResponse = await fetch(event.request.url, { mode: 'no-cors' })
-      if (!fetchResponse.ok) throw fetchResponse.statusText
-      await cache.put(event.request, fetchResponse.clone())
-      return fetchResponse
+      // const fetchResponse = await fetch(event.request.url, { mode: 'no-cors' })
+      // if (!fetchResponse.ok) throw fetchResponse.statusText
+      // await cache.put(event.request, fetchResponse.clone())
+      // return fetchResponse
+      console.log(fetch(event.request.url, { mode: 'no-cors' }))
+      return fetch(event.request.url, { mode: 'no-cors' })
     }
 
     return fetch(event.request.url)
